725e022bfe5c9b2d5cc5f7b79ce6e691d63900d4
AuraSalomaa <aura@salomaa.info>
2024-02-06 09:18:15 +0200

    s

--- speksit.js
+++ speksit.js
@@ -8,7 +8,7 @@ const sivuy = innerWidth
 const now = new Date()
 const tunnit = now.getHours()
 const minuutit = now.getMinutes()
-const paivamaara = new Date(Date.UTC(2023,12,26))
+const paivamaara = now.getDate()
 const options = {
     year: 'numeric',
     month: 'long',
@@ -21,4 +21,4 @@ const suomipv = paivamaara.toLocaleDateString('fi-FI', options)
 document.querySelector("#Kieli").innerText = `Järjestelmä kieli: ${language}`;
 document.querySelector("#resolution").innerText = `Näytön koko: ${resolutionx} x ${resolutiony}`;
 document.querySelector("#Reso").innerText = `Sivun koko: ${sivux} x ${sivuy}`
-document.querySelector("#Aika").innerText = `Päivä ja aika: ${suomipv} ja Klo on: ${tunnit}:${minuutit}`
+document.querySelector("#Aika").innerText = `Päivä ja aika: ${paivamaara} ja Klo on: ${tunnit}:${minuutit}`
