d1ececffdcb32c001c2b88d307e9acea67f70ba4
AuraSalomaa <aura@salomaa.info>
2024-02-21 16:38:16 +0200

    tehtävä tehty

--- viteserveri/itse-vite_serveri/vite-project/paivakirja.js
+++ viteserveri/itse-vite_serveri/vite-project/paivakirja.js
@@ -1,15 +1,15 @@
-const nappi = document.getElementById("nappi")
+const nappi = document.getElementById("paivanappi")
 
 nappi.addEventListener("click",  async function(evt){
     try {
-    const response = await fetch('pics.json');   
+    const response = await fetch('mockdata.json');   
     if (!response.ok) throw new Error('huono haku!')           // starts the download.
     const images = await response.json(); 
     
     console.log(images)    
     images.forEach(element => {
-        console.log(`Nimi: ${element.name}`)
-        const card = document.getElementById("")
+        console.log(`Nimi: ${element.day}`)
+        const card = document.querySelector(".card")
         const figure = document.createElement("figure")
         const img = document.createElement("img")
         const figcaption = document.createElement("figcaption")
@@ -18,15 +18,14 @@ nappi.addEventListener("click",  async function(evt){
         figure.appendChild(figcaption)
         
         img.src = element.address
-        img.alt = element.description
-        const node = document.createTextNode(element.name)
+        const node = document.createTextNode(element.description)
         figcaption.appendChild(node)
         
         
         
         
     });                // convert the loaded text JSON into a JavaScript object / array
-    card.innerHTML = '';
+        card.innerHTML = '';
     
     // 'address' property of the second object object in the 'images' array
 
