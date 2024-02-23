b1d03ed69cc890b65690512d3aca58583e0fdfb2
AuraSalomaa <aura@salomaa.info>
2024-02-23 12:36:45 +0200

    päivityksiä

--- viteserveri/itse-vite_serveri/vite-project/paivakirja.js
+++ viteserveri/itse-vite_serveri/vite-project/paivakirja.js
@@ -4,28 +4,32 @@ nappi.addEventListener("click",  async function(evt){
     try {
     const response = await fetch('mockdata.json');   
     if (!response.ok) throw new Error('huono haku!')           // starts the download.
-    const images = await response.json(); 
+    const entries = await response.json(); 
     
-    console.log(images)    
-    images.forEach(element => {
-        console.log(`Nimi: ${element.day}`)
-        const card = document.querySelector(".card")
+    console.log(entries)    
+    entries.forEach(element => {
+        const section = document.querySelector(".card-area")
         const figure = document.createElement("figure")
         const img = document.createElement("img")
+        const date = document.createElement("h4")
         const figcaption = document.createElement("figcaption")
-        card.appendChild(figure)
+        section.appendChild(figure)
         figure.appendChild(img)
         figure.appendChild(figcaption)
-        
+        figure.appendChild(date)
         img.src = element.address
+        const node3 = document.createTextNode(element.day)
         const node = document.createTextNode(element.description)
+        
+        date.appendChild(node3)
+        figcaption.appendChild(date)
         figcaption.appendChild(node)
         
         
         
         
     });                // convert the loaded text JSON into a JavaScript object / array
-        card.innerHTML = '';
+        section.innerHTML = '';
     
     // 'address' property of the second object object in the 'images' array
 
